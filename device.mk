08eab159a9ac3292268b31858db936dcd8c85708
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-05-01 21:59:26 -0300

    e610: Remove unused things
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- device.mk
+++ device.mk
@@ -24,8 +24,6 @@ $(call inherit-product, device/lge/msm7x27a-common/nfc.mk)
 
 $(call inherit-product-if-exists, vendor/lge/e610/e610-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/lge/e610/overlay
-
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 PRODUCT_AAPT_CONFIG := normal mdpi hdpi
