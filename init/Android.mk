e708f9f7c1c5ea8b962b39afb10124ff65b90b19
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-28 22:34:57 -0300

    init: Build init as C++
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
-LOCAL_SRC_FILES := init_m4.c
+LOCAL_SRC_FILES := init_m4.cpp
 LOCAL_MODULE := libinit_m4
 include $(BUILD_STATIC_LIBRARY)
